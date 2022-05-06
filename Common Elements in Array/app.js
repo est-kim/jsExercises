0f50dec6c92e949b9b39b25bebf778f84b799f3c
Esther Kim <kim.esther117@gmail.com>
2022-05-06 16:28:53 -0700

    adding first exercises

---
+++ Common Elements in Array/app.js
@@ -0,0 +1,34 @@
+// const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const arr2 = [8, 9, 10, 11, 12, 13, 14, 15];
+
+const arr1 = [true, "orange", "APPLE", "pineapple"];
+const arr2 = [false, "orange", "apple", "pineapple", true];
+
+// const arr1 = ["1","2",3];
+// const arr2 = [2, 4, 6];
+
+const filteredArray = arr1.filter(meow => arr2.includes(meow));
+
+//write a function that takes in 2 arrays and returns an array with the common items that exist in both
+const commonElements = (arr1, arr2) => {
+    let solution = []
+    for (let i = 0; i < arr1.length; i++) {
+        // arr1[i]
+        // arr1.indexOf(1) = 'orange'
+        console.log(arr2.indexOf(arr1[i]))
+        // console.log(arr2.indexOf(i))
+        if ((arr2.indexOf(arr1[i])) != -1) {
+            solution.push(arr1[i])
+        } 
+    }
+    return solution
+}
+
+// var test1 = commonElements([1,2,3], [2, 4, 6]) 
+// console.log(test1) // [2]
+
+// var test2 = commonElements(["1","2",3], [2, 4, 6]) 
+// console.log(test2) // []
+
+// var test3 = commonElements([true, "orange", "APPLE", "pineapple"], [false, "orange", "apple", "pineapple"]) 
+// console.log(test3) // ["orange", "pineapple"]
---
+++ Training/app.js
@@ -0,0 +1,138 @@
+// const a = ''
+// const b = ''
+
+// function sameCase(a, b) {
+//      //if either c OR d are not letters return -1
+//      if(a.toUpperCase() === a.toLowerCase() || b.toUpperCase() === b.toLowerCase()) {
+//         return -1
+//      }
+//     //if c and d are are same case letters, return 1
+//     else if ((a == a.toUpperCase() && b == b.toUpperCase()) || (a == a.toLowerCase() && b == b.toLowerCase())) {
+//         return 1
+//         //if c and d are letters, but NOT same case, return 0
+//     } else {
+//         return 0
+//     }
+// }
+
+// function removeChar(str) {
+//     return str.slice(1, -1)
+// }
+
+// const arr = [-2, 1, 2, 3]
+// let positiveSum = (arr) => {
+//     let sum = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < 0) { 
+//             arr[i] = 0
+//             sum += arr[i]
+//         }
+//         return sum
+//     }
+// }
+
+
+// EXERCISE: You get an empty array of numbers, return the sum of all of the positive ones
+// // If there is nothing to sum, the sum is default to 0
+// const arr = []
+// function positiveSum(arr) {
+//     let negatives = [];
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr.length < 0) {
+//             sum = 0
+//         } else if (arr[i] < 0) {
+//         negatives.push(arr[i]);
+//       } else {
+//         sum += arr[i];
+//       }
+//     }
+//     return sum;
+//   }
+
+//   //BETTER PRACTICE METHOD BELOW: 
+//   function positiveSum(arr) {
+//     let total = 0;    
+//     for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+//       if (arr[i] > 0) {                   // if arr[i] is greater than zero
+//         total += arr[i];                  // add arr[i] to total
+//       }
+//     }
+//     return total;                         // return total
+//   }
+
+//EXERCISE: You are given a number and have to make it negative. If the number is already negative, no change required.
+
+// // function makeNegative(num) {
+// //     if (num > 0) {
+// //         return -num
+// //     } else if (num < 0) {
+// //         return num
+// //     } else {
+// //         return 0
+// //     }
+// // }
+
+// BETTER PRACTICE METHOD BELOW: 
+// function makeNegative(num) {   
+//     return -Math.abs(num);   // Math.abs(-1) is 1 
+//   }
+
+// EXERCISE: Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// function boolToWord(bool) {
+//     if (bool == true) {
+//         return 'Yes'
+//     } else 
+//     return 'No'
+// }
+
+// EXERCISE: Simple, remove the spaces from the string, then return the resultant string.
+// x = ''
+
+// function noSpace(x) {
+//     console.log(x)
+//     return x.replaceAll(' ', '').trim();
+// }
+
+// EXERCISE: Given an integer or a floating-point number, find its opposite.
+// function opposite(number) {
+//     if (number < 0) {
+//         return Math.abs(number)
+//     }    
+//     return -Math.abs(number)
+//   }
+
+// //BETTER PRACTICE METHOD BELOW: 
+// function opposite(number) {
+//     return(-number);
+//   }
+
+// // EXERCISE: Complete the solution so that it reverses the string passed into it.
+// const str = ''
+
+// function solution(str){
+//     return str.split('').reverse().join('') //.split('') returns a new array of split elements; .reverse() returns reversed array; .join('') joins all elements back into string
+//   }
+
+// // EXERCISE: Convert a number to a string
+// function numberToString(num) {
+//     return `${num}`;
+//   }
+
+//   //BETTER PRACTICE METHOD BELOW: 
+// function numberToString(num) {
+//     return num.toString();
+// }
+
+//EXERCISE : Write a function called repeatStr which repeats the given string exactly n times. 
+//Example: RepeatStr(5, "Hello") // "HelloHelloHelloHelloHello" 
+
+function repeatStr (n, s) {
+    let newStr = '' //creating new empty string
+    while (n > 0) {
+        newStr += s // same as newStr = newStr + s
+        n -- // subtracting 1 each time it loops
+    }
+    return newStr
+  }
