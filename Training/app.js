e58819a6a8b960f7bf8a59f9e543cc4945f31054
Esther Kim <kim.esther117@gmail.com>
2022-05-11 19:44:14 -0700

    String Manipulation Exercise

--- Training/app.js
+++ Training/app.js
@@ -224,13 +224,28 @@
 
 //EXERCISE: Whenever an input event is fired on the input element, update the h1 so that it displays 'Welcome, ' plus the current value from
 // the text input. If the input goes back to being empty, update the h1 so that it once again says 'Enter Your Username'
-const input = document.querySelector('input')
-const h1 = document.querySelector('h1')
+// const input = document.querySelector('input')
+// const h1 = document.querySelector('h1')
 
 
-input.addEventListener('input', function (e) {
-    h1.innerText = `Welcome, ${input.value}`
-    if (input.value === '') {
-        h1.innerText = 'Enter Your Username'
-    }
-}) 
+// input.addEventListener('input', function (e) {
+//     h1.innerText = `Welcome, ${input.value}`
+//     if (input.value === '') {
+//         h1.innerText = 'Enter Your Username'
+//     }
+// }) 
+
+//EXERCISE: Create a function which translates a given DNA string into RNA.
+// Example: 'GCAT' => 'GCAU'
+
+// let dna = ''
+// function DNAtoRNA(dna) {
+//     return dna.replaceAll('T', 'U')
+// }
+
+//using regex
+// let dna = ''
+// function DNAtoRNA(dna) {
+//     return dna.replace(/T/g, 'U')
+// }
+
