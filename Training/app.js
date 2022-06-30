4bad6e80c34a2d769449aa8153abebbf2ab8e68a
Esther Kim <kim.esther117@gmail.com>
2022-06-29 19:47:01 -0700

    Regex practice function

--- Training/app.js
+++ Training/app.js
@@ -314,3 +314,11 @@
 // function greet(name){
 //     return `Hello, ${name} how are you doing today?`
 // }
+
+//EXERCISE: Return the number (count) of vowels in the given string.
+//We will consider a, e, i, o, u as vowels for this Kata (but not y).
+//The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+    return str.replace(/[^aeiou]/gi, "").length;
+}
