2446f7766b2941fd5f4aa40f50bea1e290298b74
Esther Kim <kim.esther117@gmail.com>
2022-07-15 15:11:12 -0700

    array method

--- Training/app.js
+++ Training/app.js
@@ -371,3 +371,23 @@
 //             return "odd"
 //         }
 // }
+
+//EXERCISE: Make a program that filters a list of strings and returns a list with only your friends name in it.
+//If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// function friend(friends) {
+//     let list = []
+//     for (let i = 0; i < friends.length; i++) {
+//         if (friends[i].length = 4) {
+//             list.push(friends[i])
+//         }
+//     }
+// }
+
+function friend(friends){
+    // let friends = ["Apple", "Two", "Yesterday", "mother", "lol", "car", "computer"];
+    console.log(friends.filter(function(item){
+          return item.length=4;
+    }));
+}
+friend();
