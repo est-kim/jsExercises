eb2fd70d1e5f229daf0e4275aefc599e6336a9c1
Esther Kim <kim.esther117@gmail.com>
2022-07-21 22:50:40 -0700

    String manipulation function

--- Training/app.js
+++ Training/app.js
@@ -384,10 +384,20 @@
 //     }
 // }
 
-function friend(friends){
-    // let friends = ["Apple", "Two", "Yesterday", "mother", "lol", "car", "computer"];
-    console.log(friends.filter(function(item){
-          return item.length=4;
-    }));
-}
-friend();
+// function friend(friends){
+//     // let friends = ["Apple", "Two", "Yesterday", "mother", "lol", "car", "computer"];
+//     console.log(friends.filter(function(item){
+//           return item.length=4;
+//     }));
+// }
+// friend();
+
+//EXERCISE: Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+//Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+const spinWords = str => str
+  .split(' ')
+  .map(word => word.length >= 5
+    ? [...word].reverse().join('')
+    : word)
+  .join(' ')
