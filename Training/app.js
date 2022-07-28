45790da931ed94980ff76c830ffe87f51eee48b1
Esther Kim <kim.esther117@gmail.com>
2022-07-27 22:57:20 -0700

    DNA Complementary Strand Function

--- Training/app.js
+++ Training/app.js
@@ -395,9 +395,22 @@
 //EXERCISE: Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
 //Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
-const spinWords = str => str
-  .split(' ')
-  .map(word => word.length >= 5
-    ? [...word].reverse().join('')
-    : word)
-  .join(' ')
+// const spinWords = str => str
+//   .split(' ')
+//   .map(word => word.length >= 5
+//     ? [...word].reverse().join('')
+//     : word)
+//   .join(' ')
+
+//EXERCISE: In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+function DNAStrand(dna){
+  let sequence = {
+    "A": "T",
+    "T": "A",
+    "G": "C",
+    "C": "G"
+  }
+  return dna.replace(/A|T|G|C/g, function(matched){
+    return sequence[matched];
+  });
+}
