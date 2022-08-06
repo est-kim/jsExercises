acddb45a208dc4be2e3630b2f93efb6bd36f88c9
Esther Kim <kim.esther117@gmail.com>
2022-08-05 21:43:51 -0700

    isPrime function

--- Training/app.js
+++ Training/app.js
@@ -424,3 +424,7 @@ const isPrime = num => {
       if(num % i === 0) return false; 
   return num > 1;
 }
+
+
+//EXERCISE: Given an array of integers, find the one that appears an odd number of times.
+//There will always be only one integer that appears an odd number of times.
